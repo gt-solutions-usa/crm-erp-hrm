f5a9a1eaf6764f15951deb7d9588509f736c7f45
hassan-31x <muhammadhassanchannel786@gmail.com>
2024-10-23 23:50:07 +0500

    changed server urls

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default ({ mode }) => {
   const proxy_url =
     process.env.VITE_DEV_REMOTE === 'remote'
       ? process.env.VITE_BACKEND_SERVER
-      : 'http://localhost:8888/';
+      : 'http://147.79.100.200:8888/';
 
   const config = {
     plugins: [react()],
